28601516ad0e8198a83f80ad93a7c950584bb0c9
michael-lowe-nz <mikkeyfluttie@gmail.com>
2017-02-05 12:47:04 -0600

    Add the remote mongodb

--- app.js
+++ app.js
@@ -2,6 +2,19 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var dotenv = require('dotenv').config()
+var mongoose = require('mongoose')
+
+var Schema = new mongoose.Schema({
+  id: String,
+  title: String
+})
+
+mongoose.connect(process.env.MONGOLAB_URI, function (error) {
+    if (error) console.error(error);
+    else console.log('mongo connected');
+});
+
 
 var users = require('./routes/users');
 
