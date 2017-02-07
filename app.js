d586974f24685ed498a0a31edf042432e27ef35a
michael-lowe-nz <mikkeyfluttie@gmail.com>
2017-02-06 23:41:50 -0600

    Add route for transactions, set up transaction schema

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var express = require('express')
+var path = require('path')
+var cookieParser = require('cookie-parser')
+var bodyParser = require('body-parser')
 var dotenv = require('dotenv').config()
 var mongoose = require('mongoose')
 
@@ -10,20 +10,36 @@ var Schema = new mongoose.Schema({
   title: String
 })
 
+var Schema = new mongoose.Schema({
+  id: Number,
+  trip : String,
+  location: String,
+  date: String,
+  description: String,
+  category: String,
+  amount: Number,
+  userId: Object
+})
+
+var Transactions = mongoose.model('Transactions', Schema)
+
 mongoose.connect(process.env.MONGOLAB_URI, function (error) {
-    if (error) console.error(error);
-    else console.log('mongo connected');
-});
+    if (error) console.error(error)
+    else console.log('mongo connected')
+})
 
 
-var users = require('./routes/users');
 
-var app = express();
+var users = require('./routes/users')
+var transactions = require('./routes/transactions')
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+var app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
 
-app.use('/api/v1/users', users);
+app.use('/api/v1/users', users)
+app.use('/api/v1/transactions', transactions)
 
-module.exports = app;
+module.exports = app
